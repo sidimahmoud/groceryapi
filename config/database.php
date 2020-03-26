9a438c0ca8276a41ec299ad28aa1024dfb815c9e
Mokhtar sidi mahmoud <sidmah10@gmail.com>
2020-03-26 18:47:14 +0000

    Update database connection

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Str;
 
 return [
-
+    $DATABASE_URL=parse_url('postgres://vsgalyzeyaiops:20756be2459955a42e94bdd9ec1989adb502a58f448dfb193ba0ab044eb70752@ec2-3-234-169-147.compute-1.amazonaws.com:5432/deh0a95vkf0809'),
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => $DATABASE_URL,
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
