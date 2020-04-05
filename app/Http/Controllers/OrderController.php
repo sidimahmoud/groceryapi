a887953cac4dec71401a1a7aacf504e998bf71bc
Mokhtar sidi mahmoud <sidmah10@gmail.com>
2020-04-05 10:06:19 +0000

    show order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repository\Eloquent\OrderRepository;
+use App\Order;
 
 class OrderController extends Controller
 {
@@ -30,5 +31,19 @@ class OrderController extends Controller
     {
         $orders = $this->orderRepository->create($request->all());
         return response()->json($orders, 201);
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param Order $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Int $id)
+    {
+        // This is redundant, I know. But this is the easiest way for now
+        // to still use the query builder that's automatically loaded using this method.
+        $order = $this->orderRepository->findById($id);
+        
+        return response()->json($order);
     }
 }
